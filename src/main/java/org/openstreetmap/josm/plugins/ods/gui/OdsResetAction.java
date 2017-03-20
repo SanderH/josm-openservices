fb3c95ce02a628af5f8b4892ed6c9d3146398b9b
gidema <mail@gertjanidema.nl>
2017-03-20 21:50:43 +0100

    Fixed #51 Warn about pending changes on reset

--- src/main/java/org/openstreetmap/josm/plugins/ods/gui/OdsResetAction.java
+++ src/main/java/org/openstreetmap/josm/plugins/ods/gui/OdsResetAction.java
@@ -2,8 +2,12 @@ package org.openstreetmap.josm.plugins.ods.gui;
 
 import java.awt.event.ActionEvent;
 
+import javax.swing.JOptionPane;
+
+import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.plugins.ods.OdsModule;
 import org.openstreetmap.josm.plugins.ods.exceptions.OdsException;
+import org.openstreetmap.josm.tools.I18n;
 
 public class OdsResetAction extends OdsAction {
 
@@ -19,11 +23,23 @@ public class OdsResetAction extends OdsAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        try {
-            module.reset();
-        } catch (OdsException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
+        boolean reset = true;
+        if (module.getOsmLayerManager().getOsmDataLayer().requiresUploadToServer()) {
+            String title = I18n.tr("Are you sure?");
+            String message = I18n.tr(
+                "There are unsaved changes on the OSM layer.\n" +
+                "Are you sure you want to reset and lose these changes?");
+            int answer = JOptionPane.showOptionDialog(Main.panel, message, title, JOptionPane.OK_CANCEL_OPTION,
+                JOptionPane.WARNING_MESSAGE, null, null, JOptionPane.CANCEL_OPTION);
+            reset = (answer == JOptionPane.OK_OPTION);
+        }
+        if (reset) {
+            try {
+                module.reset();
+            } catch (OdsException e1) {
+                // TODO Better exception handling
+                e1.printStackTrace();
+            }
         }
     }
 }
